37d16b650447ecd811919207b52cc7d197de7b2c
andy <andy_lib@gmx.de>
2021-10-10 16:55:22 +0200

    added interfaces

---
+++ ts/interfaces.ts
@@ -0,0 +1,19 @@
+'use strict';
+
+interface Rezept {
+    id: number;
+    gericht: string;
+    kueche: string;
+    gang: string;
+    zutaten: Array<Zutat>;
+    zubereitung: string;
+}
+
+interface Zutat {
+    id: number;
+    menge: string;
+    zutat: string;
+    foreign_key: number;
+}
+
+export { Rezept, Zutat }
