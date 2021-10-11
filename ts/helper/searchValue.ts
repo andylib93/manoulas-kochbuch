7747e1dae98f41fff04106bde5ed2d8eda933f2c
andy <andy_lib@gmx.de>
2021-10-11 10:34:38 +0200

    optics

--- ts/helper/searchValue.ts
+++ ts/helper/searchValue.ts
@@ -1,14 +1,12 @@
 import { Rezept } from '../interfaces.js';
 
-const searchValue = (array: Array<Rezept>, event: Event): Array<Rezept> => {
-    
-    let filtered: Array<Rezept> = [];
+const searchValue = (array: Rezept[], event: Event): Rezept[] => {
+    let filtered: Rezept[] = [];
     
     array.forEach(recipe => {
         const target = event.target as HTMLInputElement;
         if (recipe.gericht.toLowerCase().match(target.value.toLowerCase())) filtered.push(recipe);
     });
-    
     return filtered;
 }
 
